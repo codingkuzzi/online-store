c69b875ca92370468d59d7206c40205eb1411a78
Guest User <Guest@epicodus-94.local>
2017-08-22 15:12:52 -0700

    fix gitignore

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# OnlineStore
+## OnlineStore
+
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0.
 
---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyB97KyEncFzl2As84hfj2a_icmPxXiqBgE",
+  authDomain: "online-store-46a0a.firebaseapp.com",
+  databaseURL: "https://online-store-46a0a.firebaseio.com",
+  projectId: "online-store-46a0a",
+  storageBucket: "online-store-46a0a.appspot.com",
+  messagingSenderId: "1054604371894"
+  };
